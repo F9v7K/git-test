45d8c1501cc005398fa3ea689eab9d8631707902
ardiansyah <ian.17bussines@gmail.com>
2025-08-01 12:38:10 +0700

    membuat file css

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Remote Git -> Github</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
