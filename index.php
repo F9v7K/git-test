40a2cfc68104061fc580f675262083fe81b8fb3b
ardiansyah <ian.17bussines@gmail.com>
2025-08-01 12:22:04 +0700

    membuat paragraf baru index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit sit tempora qui quisquam sint repellat ullam debitis! Enim tempore, consequuntur omnis id, culpa quaerat debitis est accusantium esse laboriosam ipsa.</p>
 </body>
 
 </html>
