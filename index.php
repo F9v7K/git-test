71fb4b72d89e2c6dc55314892255c47921328893
ardiansyah <ian.17bussines@gmail.com>
2025-08-01 12:29:49 +0700

    menambahkan list index.php

--- index.php
+++ index.php
@@ -10,6 +10,11 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit sit tempora qui quisquam sint repellat ullam debitis! Enim tempore, consequuntur omnis id, culpa quaerat debitis est accusantium esse laboriosam ipsa.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
 </body>
 
 </html>
